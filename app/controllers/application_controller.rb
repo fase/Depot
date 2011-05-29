dd9373b626b10574e5dfccb80c927501fe5778a0
jtf <jfasenmyer@gmail.com>
2011-05-28 22:42:57 -0400

    Finished Chapter 9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
+  def reset_counter
+    session[:counter] = nil if !session[:counter].nil?
+  end
+
+  def update_counter
+    counter = 1
+
+    if !session[:counter].nil?
+      counter = session[:counter] + 1
+    end
+
+    session[:counter] = counter
+
+    counter
+  end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @visits = update_counter
   end
-
 end
